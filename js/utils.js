bac15687fb7511739fc18b0ea9158cfed5fb69f5
reshnerp-commits <reshnerp@gmail.com>
2026-02-28 03:19:41 +0800

    Create utils.js

---
+++ js/utils.js
@@ -0,0 +1,6 @@
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    document.getElementById('toastMessage').textContent = message;
+    toast.classList.add('show');
+    setTimeout(() => toast.classList.remove('show'), 3000);
+}
